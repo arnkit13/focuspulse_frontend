23706494b4c6cbac91ce09a8d135f5402e2d7e98
ginmurin <johnemmanuel.sevilla@cit.edu>
2026-03-15 00:39:38 +0800

    Initialized Springboot

---
+++ focuspulse/src/main/java/edu/cit/cabigas/focuspulse/FocuspulseApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.cabigas.focuspulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocuspulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocuspulseApplication.class, args);
+	}
+
+}
---
+++ focuspulse/src/test/java/edu/cit/cabigas/focuspulse/FocuspulseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.cabigas.focuspulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocuspulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
